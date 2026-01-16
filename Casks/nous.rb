5fd61062eb730bac5ee0e54891dc0e21ff8fd14f
arjunnai <arjunnair2k18@gmail.com>
2026-01-16 12:00:36 -0500

    Update nous.rb with correct sha256 comment and formatting adjustments

--- Casks/nous.rb
+++ Casks/nous.rb
@@ -1,11 +1,20 @@
 cask "nous" do
   version "1.0.0"
-  sha256 "573ec210ac074af2dd3d95c8988c62a245326a8806bd75fac4cedee647689516" # Run 'shasum -a 256 Nous.zip' in terminal to get this
+  # REPLACE THIS WITH THE LONG STRING YOU GOT FROM THE TERMINAL COMMAND ABOVE
+  sha256 "573ec210ac074af2dd3d95c8988c62a245326a8806bd75fac4cedee647689516"
 
+  # Your exact URL
   url "https://github.com/arjunnai/nous-release/releases/download/v1.0/Nous.zip"
+
   name "Nous"
   desc "The Academic Operating System"
   homepage "https://github.com/arjunnai/nous-release"
 
   app "Nous.app"
+
+  # This bypasses the "Unidentified Developer" quarantine for your friends
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/Nous.app"]
+  end
 end
