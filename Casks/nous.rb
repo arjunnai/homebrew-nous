67a160adc3b99a64f6b51da323d20eacafca263a
arjunnai <arjunnair2k18@gmail.com>
2026-01-16 11:59:57 -0500

    Ready for v1.0 release

--- Casks/nous.rb
+++ Casks/nous.rb
@@ -1,8 +1,8 @@
 cask "nous" do
   version "1.0.0"
-  sha256 "5729835cb253d75d5e974265fe07960a9b0db2f88c43936edcca9ee1ad8f082e" # Run 'shasum -a 256 Nous.zip' in terminal to get this
+  sha256 "573ec210ac074af2dd3d95c8988c62a245326a8806bd75fac4cedee647689516" # Run 'shasum -a 256 Nous.zip' in terminal to get this
 
-  url "https://github.com/arjunnai/nous-release/blob/main/nous-1.0.zip"
+  url "https://github.com/arjunnai/nous-release/releases/download/v1.0/Nous.zip"
   name "Nous"
   desc "The Academic Operating System"
   homepage "https://github.com/arjunnai/nous-release"
