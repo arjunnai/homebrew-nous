d753b578926c4b85a44cb7ddd97837550c2c943c
arjunnai <arjunnair2k18@gmail.com>
2026-01-16 12:11:11 -0500

    Update nous.rb to version 1.0.1

--- Casks/nous.rb
+++ Casks/nous.rb
@@ -1,5 +1,5 @@
 cask "nous" do
-  version "1.0.0"
+  version "1.0.1"
   # REPLACE THIS WITH THE LONG STRING YOU GOT FROM THE TERMINAL COMMAND ABOVE
   sha256 "9e5be680056c0bd72e90f7a61d847aefd1beb465554f8bd03ac9f34850210c36"
 
